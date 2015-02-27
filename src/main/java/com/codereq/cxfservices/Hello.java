51172f6a6fc6c2a4ef3b81cbf1640c84d49c57d3
niuwanlu <niuwanlu@gmail.com>
2015-02-27 13:59:46 +0800

    add Hello.java

---
+++ src/main/java/com/codereq/cxfservices/Hello.java
@@ -0,0 +1,19 @@
+package com.codereq.cxfservices;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+@WebService
+public class Hello {
+
+    @WebMethod
+    public String hello(String name) {
+        return "Hello," + name + "\n";
+    }
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        Endpoint endpoint = Endpoint.publish("http://localhost:8080/hello",hello);
+    }
+}
---
+++ src/main/java/com/codereq/cxfservices/HelloClient.java
@@ -0,0 +1,12 @@
+//import com.codereq.cxfservices.Hello;
+//import com.codereq.cxfservices.HelloService;
+//
+//
+//public class HelloClient{
+//    public static void main(String args[]) {
+//        HelloService service = new HelloService();
+//        Hello helloProxy = service.getHelloPort();
+//        String hello = helloProxy.hello("nihao");
+//        System.out.println(hello);
+//    }
+//}
